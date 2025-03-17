6d0c2d531d88df22b771041c85c5a60eb304356c
Leo Ermebring <leo.ermebring@edu.lycksele.se>
2025-03-17 08:55:16 +0100

    Yeah

--- Generiska klasser/obj/Debug/net8.0/Generiska klasser.AssemblyInfo.cs
+++ Generiska klasser/obj/Debug/net8.0/Generiska klasser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generiska klasser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206f8ff45b3ed13a43d60a460734de6c615988b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generiska klasser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generiska klasser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
